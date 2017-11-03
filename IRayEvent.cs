8bdbe7f94590ff9a54eedef032759eebafe96888
Atsushi_Yamashita <yays505@gmail.com>
2017-11-04 03:20:42 +0900

    Create IRayEvent.cs

---
+++ IRayEvent.cs
@@ -0,0 +1,25 @@
+/// <summary>
+/// This AY Utility sets.
+/// <author>Atsushi. Yamashita.</author>
+/// </summary>
+namespace AY_Util
+{
+
+    /// <summary>
+    /// This express ray events.
+    /// </summary>
+    public enum RayEvent
+    {
+        RECIEVE,
+        REMOVE,
+        DOWN_TRIGGER,
+    }
+
+    /// <summary>
+    /// This interface express Ray event object.
+    /// <author>Atsushi. Yamashita.</author>
+    /// </summary>
+    public interface IRayEvent
+    {
+    }
+}
