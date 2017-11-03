fb82d35b29a2a5c0ef0e4e85440197d801723eae
Atsushi_Yamashita <yays505@gmail.com>
2017-11-03 16:36:42 +0900

    Create StringUtil.cs
    Add To Title Case.

---
+++ StringUtil.cs
@@ -0,0 +1,25 @@
+/// <summary>
+/// Utility library.
+/// <authority> ATSUSHI YAMASHITA. </authority>
+/// </summary>
+namespace AY_Util
+{
+    using System.Globalization;
+
+    /// <summary>
+    /// String utilities library.
+    /// </summary>
+    public static class StringUtil
+    {
+        /// <summary>
+        /// Convert to title case.
+        /// </summary>
+        /// <param name="str">Convert string.</param>
+        /// <returns>Converted string.</returns>
+        public static string ToTitleCase ( string str )
+        {
+            TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
+            return ti.ToTitleCase( str );
+        }
+    }
+}
