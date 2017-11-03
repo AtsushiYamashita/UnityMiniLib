05cb499d01031aa8b271714610d6a4239cbb8779
Atsushi_Yamashita <yays505@gmail.com>
2017-11-04 02:47:46 +0900

    Update ListUtil.cs
    Add Filter function.

--- ListUtil.cs
+++ ListUtil.cs
@@ -14,10 +14,35 @@ namespace AY_Util
     public class ListUtil<T>
     {
         /// <summary>
+        /// express filter function. when this function return false then remove.
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public delegate bool FilterFunc ( T t );
+
+        /// <summary>
         /// Express no return function.
         /// </summary>
         /// <param name="t"> a list element </param>
         public delegate void VoidFunc ( T t );
+
+        /// <summary>
+        /// this function is little heavy because every call use new.
+        /// this function will filter the list under the function.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="func"></param>
+        /// <returns>filtered list.</returns>
+        public static List<T> Filter ( List<T> list, FilterFunc func )
+        {
+            List<T> ret = new List<T>();
+            foreach (var t in list)
+            {
+                var match = func( t );
+                if (match) { ret.Add( t ); }
+            }
+            return ret;
+        }
 
         /// <summary>
         /// This wrapping a foreach in C#.
@@ -26,7 +51,7 @@ namespace AY_Util
         /// <param name="func"> Apply function. </param>
         public static void ForEach ( List<T> list, VoidFunc func )
         {
-            foreach (T t in list) { func( t ) };
+            foreach (T t in list) func( t );
         }
     }
 }
